dabf5e9e541341be7edfb43e9ae9d459731e4c64
LuisIllanes <Luis.illanes@serviciosryc.cl>
2024-08-26 13:33:19 -0400

    ionic boton hola

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private toastController: ToastController) {}
 
-  constructor() {}
+  async diHola() {
+    const toast = await this.toastController.create({
+      message: 'Hola',
+      duration: 2000,
+      position: 'middle'
+    });
+    await toast.present();
+  }
 
 }
